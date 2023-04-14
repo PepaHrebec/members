9bcfac5d5b332ca740af2695937fafc0c1093d27
Josef Hrebec <josefhrebec02@gmail.com>
2023-04-14 15:32:21 +0200

    Add sign_up post middleware

--- controllers/user_controller.js
+++ controllers/user_controller.js
@@ -1,7 +1,63 @@
+const { body, validationResult } = require("express-validator");
+const bcrypt = require("bcryptjs");
+const User = require("../models/user");
+
 exports.user_sign_up_get = (req, res, next) => {
   res.render("sign_up", {
     title: "Sign-up",
+    error: "None",
   });
 };
 
-exports.user_sign_up_post = (req, res, next) => {};
+exports.user_sign_up_post = [
+  body("username")
+    .trim()
+    .escape()
+    .isLength({ min: 3, max: 100 })
+    .withMessage("Username must be at least 3 characters."),
+  body("password")
+    .trim()
+    .escape()
+    .isLength({ min: 3, max: 100 })
+    .withMessage("Password must be at least 3 characters."),
+  body("conf_password").custom((value, { req }) => {
+    if (value !== req.body.password)
+      throw new Error("Passwords must be the same");
+    return true;
+  }),
+  (req, res, next) => {
+    const errs = validationResult(req);
+    if (!errs.isEmpty()) {
+      return res.render("sign_up", {
+        title: "Sign-up",
+        error: errs,
+      });
+    }
+
+    User.find({ username: req.body.username }).then((results) => {
+      if (results.length > 0) {
+        return res.render("sign_up", {
+          title: "Sign-up",
+          error: "User exists",
+        });
+      }
+    });
+
+    bcrypt.hash(req.body.password, 10, (err, hashedPasswd) => {
+      if (err) {
+        return next(err);
+      }
+      const user = new User({
+        username: req.body.username,
+        password: hashedPasswd,
+        status: false,
+      });
+      user
+        .save()
+        .then(() => res.redirect("/"))
+        .catch((err) => {
+          return next(err);
+        });
+    });
+  },
+];
--- models/user.js
+++ models/user.js
@@ -3,15 +3,9 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const UserSchema = new Schema({
-  first_name: { type: String, maxLength: 100, required: true },
-  last_name: { type: String, maxLength: 100, required: true },
   username: { type: String, maxLength: 100, required: true },
   password: { type: String, maxLength: 100, required: true },
-  status: { type: String },
-});
-
-UserSchema.virtual("name").get(function () {
-  return `${this.first_name}.${this.last_name}`;
+  status: { type: Boolean, required: true },
 });
 
 module.exports = mongoose.model("User", UserSchema);
